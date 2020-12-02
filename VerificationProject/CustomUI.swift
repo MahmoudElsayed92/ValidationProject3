25a310ae5424e8ed2efc93d52555bc46f017c4ed
Mahmoud <mahmouddiesel2013@gmail.com>
2020-12-02 21:35:59 +0200

    done2

--- VerificationProject/CustomUI.swift
+++ VerificationProject/CustomUI.swift
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class CustomUI: UITextField {
+class CustomUI: UITextField  {
 
     override func awakeFromNib() {
         layer.cornerRadius = 20
         layer.borderWidth = 1
         layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         clipsToBounds = true
+        
     }
-
+    
 }
--- VerificationProject/RegisterVC.swift
+++ VerificationProject/RegisterVC.swift
@@ -15,7 +15,9 @@ class RegisterVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
     }
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -28,17 +30,29 @@ class RegisterVC: UIViewController {
     }
 
     @IBAction func RegisterBu(_ sender: UIButton) {
-        
+        let userName = userNameTxt.text
+        let thePassword = PassWordTxt.text
+        let theEmail = emailTxt.text
+        let thephoneNumber = phoneNumberTxt.text
+    
         UserDefaults.standard.set(userNameTxt.text, forKey: "UserKey")
         UserDefaults.standard.set(emailTxt.text, forKey: "EmailKey")
         UserDefaults.standard.set(PassWordTxt.text, forKey: "PasswordKey")
-      
+        if userName?.isEmpty == true || thePassword?.isEmpty == true || theEmail?.isEmpty == true || thephoneNumber?.isEmpty == true  {
+            let alert = UIAlertController(title: "Attention", message: "please fill all fields", preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "OK", style:.cancel, handler: nil)
+            alert.addAction(alertAction)
+            present(alert, animated: true, completion: nil)
+            
+            
+            
+            
+        }else{
 
         self.performSegue(withIdentifier: "LoginVC", sender: self)
-        
+        }
     }
 //    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 //        let destinationVC = segue.destination as!LoginVC
 //    }
 }
-
